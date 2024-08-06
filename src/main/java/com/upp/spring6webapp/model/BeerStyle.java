a15752e03e5a57dd7d593d1b26ea903e9f697df7
uppnrise <mtbnacar@gmail.com>
2024-08-06 09:11:08 +0200

    [SB6P-3] Created Beer POJO with service and controller classes. - A test class is created.

---
+++ src/main/java/com/upp/spring6webapp/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package com.upp.spring6webapp.model;
+
+public enum BeerStyle {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/main/java/com/upp/spring6webapp/service/BeerService.java
@@ -0,0 +1,12 @@
+package com.upp.spring6webapp.service;
+
+import com.upp.spring6webapp.model.Beer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BeerService {
+    List<Beer> listBeers();
+
+    Beer getBeerById(UUID id);
+}
