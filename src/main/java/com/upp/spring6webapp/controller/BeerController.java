4671c86455fd64f25867fdd1950ee7fd78e5d560
uppnrise <mtbnacar@gmail.com>
2024-08-06 10:41:30 +0200

    [SB6P-5] Location header is added to createBeer endpoint.

--- src/main/java/com/upp/spring6webapp/controller/BeerController.java
+++ src/main/java/com/upp/spring6webapp/controller/BeerController.java
@@ -4,6 +4,7 @@ import com.upp.spring6webapp.model.Beer;
 import com.upp.spring6webapp.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+import static com.upp.spring6webapp.controller.BeerController.API_V1_BEER_PATH;
+
 @Slf4j
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/v1/beer")
+@RequestMapping(API_V1_BEER_PATH)
 public class BeerController {
+    public static final String API_V1_BEER_PATH = "/api/v1/beer";
+
     private final BeerService beerService;
 
     @RequestMapping(method = RequestMethod.GET)
@@ -34,6 +39,10 @@ public class BeerController {
     @PostMapping
     public ResponseEntity<Beer> createBeer(@RequestBody Beer beer){
         Beer saved = beerService.saveNewBeer(beer);
-        return new ResponseEntity<>(saved, HttpStatus.CREATED);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Location", API_V1_BEER_PATH + "/" + saved.getId());
+
+        return new ResponseEntity<>(saved, httpHeaders, HttpStatus.CREATED);
     }
 }
