4720ec00fac0cb79e63157c93aafddc1efe74afc
uppnrise <mtbnacar@gmail.com>
2024-08-08 23:16:03 +0200

    [SB6P-30] ExceptionController is deleted and ResponseStatus annotation is added to NotFoundException

--- src/main/java/com/upp/spring6webapp/controller/NotFoundException.java
+++ src/main/java/com/upp/spring6webapp/controller/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.upp.spring6webapp.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value Not Found!")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
