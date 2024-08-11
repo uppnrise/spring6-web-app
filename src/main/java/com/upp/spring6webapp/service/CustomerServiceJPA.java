eb40f655e81b7566cc609424eb6c957131980dfd
uppnrise <mtbnacar@gmail.com>
2024-08-11 14:41:24 +0200

    [SB6P-40] CustomerServiceJPA is created. getAllCustomers and getCustomerById methods are implemented.

---
+++ src/main/java/com/upp/spring6webapp/service/CustomerServiceJPA.java
@@ -0,0 +1,54 @@
+package com.upp.spring6webapp.service;
+
+import com.upp.spring6webapp.mappers.CustomerMapper;
+import com.upp.spring6webapp.model.CustomerDTO;
+import com.upp.spring6webapp.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.ofNullable(customerMapper.customerToCustomerDto(customerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return customerRepository.findAll().stream()
+                .map(customerMapper::customerToCustomerDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CustomerDTO createCustomer(CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO updateCustomerById(UUID customerId, CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customerDTO) {
+
+    }
+}
