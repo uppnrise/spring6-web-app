779db84678fad695a27574322f5ed82551f8d386
uppnrise <mtbnacar@gmail.com>
2024-08-11 14:39:58 +0200

    [SB6P-39] BeerServiceJPA is created. ListBeers and getBeerById methods are implemented.

---
+++ src/main/java/com/upp/spring6webapp/service/BeerServiceJPA.java
@@ -0,0 +1,54 @@
+package com.upp.spring6webapp.service;
+
+import com.upp.spring6webapp.mappers.BeerMapper;
+import com.upp.spring6webapp.model.BeerDTO;
+import com.upp.spring6webapp.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService {
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return beerRepository.findAll().stream()
+                .map(beerMapper::beerToBeerDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.ofNullable(beerMapper.beerToBeerDto(beerRepository.findById(id)
+                .orElse(null)));
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beerDTO) {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID beerId, BeerDTO beerDTO) {
+
+    }
+
+    @Override
+    public void deleteBeerById(UUID beerId) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beerDTO) {
+
+    }
+}
