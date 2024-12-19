17ee8a284cabd486038ef9b252f78c09230b2263
uppnrise <mtbnacar@gmail.com>
2024-12-19 23:55:04 +0100

    [SB6P-63] created handleJPAViolations method.

--- src/main/java/com/upp/spring6webapp/controller/CustomErrorController.java
+++ src/main/java/com/upp/spring6webapp/controller/CustomErrorController.java
@@ -1,16 +1,39 @@
 package com.upp.spring6webapp.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CustomErrorController {
+
+    @ExceptionHandler
+    ResponseEntity<Object> handleJPAViolations(TransactionSystemException exception) {
+        ResponseEntity.BodyBuilder responseEntity = ResponseEntity.badRequest();
+
+        if(exception.getCause() instanceof ConstraintViolationException) {
+            ConstraintViolationException cve = (ConstraintViolationException) exception.getCause().getCause();
+
+            List<Map<String, String>> errors = cve.getConstraintViolations().stream()
+                    .map(constraintViolation -> {
+                        Map<String, String> errMap = new HashMap<>();
+                        errMap.put(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage());
+                        return errMap;
+                    }).collect(Collectors.toList());
+
+            return responseEntity.body(errors);
+        }
+
+        return responseEntity.build();
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     ResponseEntity<Object> handleBindErrors(MethodArgumentNotValidException exception) {
