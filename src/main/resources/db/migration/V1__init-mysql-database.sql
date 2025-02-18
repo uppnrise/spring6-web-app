caed343808adb5947309357341fa46f6eb07136c
uppnrise <mtbnacar@gmail.com>
2025-02-18 13:11:57 +0100

    [SB6P-70] flyway-mysql is introduced to manage db migrations with a V1 init script.

---
+++ src/main/resources/db/migration/V1__init-mysql-database.sql
@@ -0,0 +1,5 @@
+
+drop table if exists beer cascade ;
+drop table if exists customer cascade ;
+create table beer (beer_style tinyint not null check (beer_style between 0 and 9), price numeric(38,2) not null, quantity_on_hand integer, version integer, created_date timestamp(6), update_date timestamp(6), id varchar(36) not null, beer_name varchar(50) not null, upc varchar(255) not null, primary key (id));
+create table customer (version integer, created_date timestamp(6), updated_date timestamp(6), id varchar(36) not null, name varchar(255), primary key (id));
