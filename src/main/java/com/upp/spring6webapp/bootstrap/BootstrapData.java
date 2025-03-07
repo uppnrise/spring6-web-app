da2f0df4fc9a0a8aeec6762ba9b26115b08e8bad
uppnrise <mtbnacar@gmail.com>
2025-03-07 12:23:30 +0100

    [SB6P-85] Implement CSV data loading in BootstrapData and annotate BeerCsvServiceImpl as a Spring service

--- src/main/java/com/upp/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/com/upp/spring6webapp/bootstrap/BootstrapData.java
@@ -2,16 +2,24 @@ package com.upp.spring6webapp.bootstrap;
 
 import com.upp.spring6webapp.entities.Beer;
 import com.upp.spring6webapp.entities.Customer;
+import com.upp.spring6webapp.model.BeerCSVRecord;
 import com.upp.spring6webapp.model.BeerStyle;
 import com.upp.spring6webapp.repositories.BeerRepository;
 import com.upp.spring6webapp.repositories.CustomerRepository;
+import com.upp.spring6webapp.service.BeerCsvService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -19,11 +27,45 @@ public class BootstrapData implements CommandLineRunner {
 
     private final BeerRepository beerRepository;
     private final CustomerRepository customerRepository;
+    private final BeerCsvService csvService;
 
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
         loadBeerData();
+        loadCsvData();
         loadCustomerData();
+    }
+
+    private void loadCsvData() throws FileNotFoundException {
+        if (beerRepository.count() < 10){
+            File file = ResourceUtils.getFile("classpath:csvdata/beers.csv");
+
+            List<BeerCSVRecord> recs = csvService.convertCSV(file);
+
+            recs.forEach(beerCSVRecord -> {
+                BeerStyle beerStyle = switch (beerCSVRecord.getStyle()) {
+                    case "American Pale Lager" -> BeerStyle.LAGER;
+                    case "American Pale Ale (APA)", "American Black Ale", "Belgian Dark Ale", "American Blonde Ale" ->
+                            BeerStyle.ALE;
+                    case "American IPA", "American Double / Imperial IPA", "Belgian IPA" -> BeerStyle.IPA;
+                    case "American Porter" -> BeerStyle.PORTER;
+                    case "Oatmeal Stout", "American Stout" -> BeerStyle.STOUT;
+                    case "Saison / Farmhouse Ale" -> BeerStyle.SAISON;
+                    case "Fruit / Vegetable Beer", "Winter Warmer", "Berliner Weissbier" -> BeerStyle.WHEAT;
+                    case "English Pale Ale" -> BeerStyle.PALE_ALE;
+                    default -> BeerStyle.PILSNER;
+                };
+
+                beerRepository.save(Beer.builder()
+                        .beerName(StringUtils.abbreviate(beerCSVRecord.getBeer(), 50))
+                        .beerStyle(beerStyle)
+                        .price(BigDecimal.TEN)
+                        .upc(beerCSVRecord.getRow().toString())
+                        .quantityOnHand(beerCSVRecord.getCount())
+                        .build());
+            });
+        }
     }
 
     private void loadBeerData() {
--- src/main/java/com/upp/spring6webapp/service/impl/BeerCsvServiceImpl.java
+++ src/main/java/com/upp/spring6webapp/service/impl/BeerCsvServiceImpl.java
@@ -3,12 +3,14 @@ package com.upp.spring6webapp.service.impl;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.upp.spring6webapp.model.BeerCSVRecord;
 import com.upp.spring6webapp.service.BeerCsvService;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Service
 public class BeerCsvServiceImpl implements BeerCsvService {
     @Override
     public List<BeerCSVRecord> convertCSV(File csvFile) {
